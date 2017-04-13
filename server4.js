477ae1e9248bdea35a4e140dacb8866afb68bc5c
MiaoChao <878310229@qq.com>
2017-04-14 07:47:30 +0800

    koa的基本实现：thunk，promise

--- server4.js
+++ server4.js
@@ -37,17 +37,63 @@ SimpleKoa.prototype = {
 var app = new SimpleKoa();
 
 app.use(function *(next){
+    console.log('11');
     this.body = '1';
     yield next;
+    console.log('12');
     this.body += '5';
     console.log(this.body);
 });
 app.use(function *(next){
+    console.log('21');
     this.body += '2';
     yield next;
+    console.log('22');
     this.body += '4';
 });
+app.use(thunkfunction);
+app.use(promisefunction);
 app.use(function *(next){
+    console.log('31');
     this.body += '3';
+    console.log('32');
 });
-app.listen(3000);
+
+function *thunkfunction(next) {
+    var r1 = yield asyncthunk('1');
+    console.log(r1);
+    yield next;
+    var r2 = yield asyncthunk('2');
+    console.log(r2);
+}
+
+function asyncthunk(str) {
+    return function (callback){
+        // do something 
+        console.log('thunk function');
+        setTimeout(function(){
+            var r = 'this is test : ' + str;
+            callback(null,r);
+        },3000);
+    }
+}
+
+function *promisefunction(next) {
+    var r1 = yield asyncpromise('1');
+    console.log(r1);
+    yield next;
+    var r2 = yield asyncpromise('2');
+    console.log(r2);
+}
+
+function asyncpromise(str){
+    return new Promise(function(resolve,reject){
+        var r = 'this is promise : ' + str;
+        setTimeout(()=>{
+            resolve(r);
+        },3000);
+        
+    });
+}
+
+app.listen();
